6b9360784d0588807835659a3debd74598bf1097
allefhmedeiros <allefhmedeiros@gmail.com>
2025-02-25 17:43:07 -0300

    Implementação do objeto Curtida que irá gerenciar o relacionamento de muitos para muitos entre usuário e publicação.

---
+++ src/main/java/br/com/encurtandocaminhos/api/model/Curtida.java
@@ -0,0 +1,54 @@
+package br.com.encurtandocaminhos.api.model;
+
+public class Curtida {
+    private Long id;
+    private Usuario usuario;
+    private Publicacao publicacao;
+    private boolean curtida = false;
+
+    public void alternarCurtida(){
+        setCurtida(!getCurtida());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Publicacao getPublicacao() {
+        return publicacao;
+    }
+
+    public void setPublicacao(Publicacao publicacao) {
+        this.publicacao = publicacao;
+    }
+
+    public boolean getCurtida() {
+        return curtida;
+    }
+
+    public void setCurtida(boolean curtida) {
+        this.curtida = curtida;
+    }
+
+    @Override
+    public String toString() {
+        return "Curtida{" +
+                "id=" + id +
+                ", usuario=" + usuario +
+                ", publicacao=" + publicacao +
+                ", curtida=" + curtida +
+                '}';
+    }
+}
