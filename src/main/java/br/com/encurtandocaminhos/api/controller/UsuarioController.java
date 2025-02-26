caadebd9c9202bb2373ce9116a5a871c5678de6a
allefhmedeiros <allefhmedeiros@gmail.com>
2025-02-26 01:05:42 -0300

    Implantada a rota de adicionar novo usuário.

--- src/main/java/br/com/encurtandocaminhos/api/controller/UsuarioController.java
+++ src/main/java/br/com/encurtandocaminhos/api/controller/UsuarioController.java
@@ -3,32 +3,38 @@ package br.com.encurtandocaminhos.api.controller;
 import br.com.encurtandocaminhos.api.model.Usuario;
 import br.com.encurtandocaminhos.api.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
     //ENDPOINT PARA BUSCAR UM USUÁRIO PELO ID
-    @GetMapping("/usuarios/{id}")
+    @GetMapping("{id}")
     public Usuario getUsuarioById(@PathVariable Long id){
         return usuarioService.buscarUsuarioPorId(id);
     }
 
-    @GetMapping("/usuarios")
+    @GetMapping
     public List<Usuario> getUsuarios(){
         return usuarioService.getAllUsuarios();
     }
 
-    @DeleteMapping("/usuarios/{id}")
+    @DeleteMapping("{id}")
     public void deleteUsuario(@PathVariable Long id){
         usuarioService.deleteUsuario(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> adicionarUsuario(@RequestBody Usuario usuario){
+        Usuario usuarioSalvo = usuarioService.salvarUsuario(usuario);
+        return new ResponseEntity<>(usuarioSalvo, HttpStatus.CREATED); //RETORNARÁ 201 COM O USUÁRIO SALVO
     }
 
 }
